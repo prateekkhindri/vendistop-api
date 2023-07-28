bbf5403a94607fe2d3083513f2799cf473ae018e
PrateekKhindri <prateek.khindri90@gmail.com>
2023-07-28 15:20:56 +1000

    T3A2-B-019-Added ordersRouter and all endpoints [Prateek]

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import registerLoginRouter from "./src/routers/registerLoginRouter.js";
 import categoryRouter from "./src/routers/categoryRouter.js";
 import productRouter from "./src/routers/productRouter.js";
 import cartsRouter from "./src/routers/cartsRouter.js";
+import ordersRouter from "./src/routers/ordersRouter.js";
 
 const app = express();
 
@@ -38,6 +39,7 @@ app.use("/api/v1/admin", userRouter);
 app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/cart", adminAuth, cartsRouter);
+app.use("/api/v1/orders", adminAuth, ordersRouter);
 
 app.get("/", (req, res) => {
   res.json({
