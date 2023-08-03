86c388cd7567074bde81d7861040ea7de613728c
PrateekKhindri <prateek.khindri90@gmail.com>
2023-08-03 23:06:23 +1000

    T3A2-B-011-Modified emailHelper.js [Prateek]

--- src/helpers/emailHelper.js
+++ src/helpers/emailHelper.js
@@ -7,8 +7,8 @@ const sendMail = async (emailInfo) => {
     port: +process.env.MAIL_PORT,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: process.env.MAIL_USER, // generated ethereal user
-      pass: process.env.MAIL_PASS, // generated ethereal password
+      user: process.env.MAIL_USER, // SMTP user
+      pass: process.env.MAIL_PASS, // SMTP password
     },
   });
 
@@ -17,10 +17,6 @@ const sendMail = async (emailInfo) => {
 
   console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 };
 
 export const profileUpdateNotification = (userObj) => {
